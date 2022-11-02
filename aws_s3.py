9d28aab7b855a3e10e08daa5ea0ff234e6442841
pablohm83 <84983170+pablohm83@users.noreply.github.com>
2022-11-02 13:17:30 -0300

    Update aws_s3.py

--- aws_s3.py
+++ aws_s3.py
@@ -191,7 +191,9 @@ if __name__=='__main__':
         elif option == 5:
             list_buckets()
             BUCKET_NAME = input('Ingrese el nombre del bucket desde donde se bajara el achivo: ')
-            list_files_bucket(BUCKET_NAME)
+            out=list_files_bucket(BUCKET_NAME)
+            if not out:
+                continue
             FILE_NAME_DOWNLOAD = input('Ingrese el nombre del archivo a bajar: ')
             path_download=input('Ingrese el path destino: ')
             download_file(path_download, FILE_NAME_DOWNLOAD,BUCKET_NAME)
